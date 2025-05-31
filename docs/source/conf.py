72e5dbdcd1d47cb86c79450bb86d52467c5e4104
Iver Smage <iversmage@gmail.com>
2025-05-31 18:20:04 +0200

    add conf.py

---
+++ docs/source/conf.py
@@ -0,0 +1,43 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sys
+
+sys.path.append("../../")
+
+from importlib.metadata import version as imversion
+
+project = "crysm"
+copyright = "2025, Iver Karlsbakk Småge"
+author = "Viljar Femoen"
+version = imversion("crysm")
+release = imversion("crysm")
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.viewcode",
+    "nbsphinx",
+]
+
+
+autosummary_generate = True  # Turn on sphinx.ext.autosummary
+
+templates_path = ["_templates"]
+exclude_patterns: list[str] = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
